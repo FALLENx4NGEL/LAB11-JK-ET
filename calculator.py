5e902bfd3b2a88877ce616d911e9777c0a352211
FALLENx4NGEL <kilpatrick.jd@ufl.edu>
2026-04-08 12:41:36 -0400

    Added Square Root & Hypotenuse functions

--- calculator.py
+++ calculator.py
@@ -1,11 +1,23 @@
 import math
 
+def square_root(a):
+    if a < 0:
+        raise ValueError
+    else:
+        return math.sqrt(a)
+
+def hypotenuse(a,b):
+    return math.hypotenuse(a,b)
+
 def add(a, b): 
     return a + b
+
 def sub(a,b):
     return a - b
+
 def mul(a,b):
     return a * b
+
 def div(a,b):
     if b == 0:
         raise ZeroDivisionError
