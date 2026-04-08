9b91f232b3f15f69374cb0035ba1e875750a1cbd
FALLENx4NGEL <kilpatrick.jd@ufl.edu>
2026-04-08 12:33:34 -0400

    Added base functions Add, Subtract, Multiply, Div, Log, and Exponent

--- calculator.py
+++ calculator.py
@@ -6,6 +6,25 @@ One function per operation, in order.
 """
 # First example
 def add(a, b): 
-    pass
+    return a + b
 
+def subtract(a, b):
+    return a - b
 
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b / a == 0:
+        raise ZeroDivisionError("Division by zero")
+    else:
+        return b / a
+
+def logarithm(a, b):
+    if math.log(a,b) < 0:
+        raise ValueError
+    else:
+        return math.log(a,b)
+
+def exponent(a, b):
+    return a ** b
